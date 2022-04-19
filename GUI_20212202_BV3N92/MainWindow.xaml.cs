244508d5ecbc88f0e20111c26dcaffcd9a1ca2d9
zsiroszsombor <zsiroszsombor02@gmail.com>
2022-04-19 20:54:19 +0200

    Implemented Display

--- GUI_20212202_BV3N92/MainWindow.xaml.cs
+++ GUI_20212202_BV3N92/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GUI_20212202_BV3N92.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,21 @@ namespace GUI_20212202_BV3N92
         public MainWindow()
         {
             InitializeComponent();
+            GameLogic logic = new GameLogic();
+            display.SetupModel(logic);
+
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            display.Resize(new Size(grid.ActualWidth, grid.ActualHeight));
+            display.InvalidateVisual();
         }
     }
 }
