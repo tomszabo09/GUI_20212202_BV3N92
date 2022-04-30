ee1830a742aba10256e35fdd48591a8aa5a561f4
tomt09 <tom.szabo09@gmail.com>
2022-04-30 13:04:17 +0200

    map is now made up of rectangles instead of fixed matrix blocks

--- GUI_20212202_BV3N92/Renderer/Display.cs
+++ GUI_20212202_BV3N92/Renderer/Display.cs
@@ -24,56 +24,63 @@ namespace GUI_20212202_BV3N92.Renderer
         {
             this.model = model;
         }
+        public Brush FloorBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "blank.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush PlayerBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "player.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush WallBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "wall.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush AmmoBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "ammosingle.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush OpponentBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "opponent.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush BrickBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "brick.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush HealthBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "hp.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush LockedBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "lock.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush ExitBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "exit.bmp"), UriKind.RelativeOrAbsolute))); } }
+        public Brush FinishBrush { get { return new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "finish.bmp"), UriKind.RelativeOrAbsolute))); } }
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
             if (model != null && size.Width>50 && size.Height>50)
             {
-                double itemWidth = size.Width / model.Map.GetLength(1);
-                double itemHeight = size.Height / model.Map.GetLength(0);
+                drawingContext.DrawRectangle(FloorBrush, null, new Rect(0, 0, size.Width, size.Height));
+                double rectWidth = size.Width / model.Map.GetLength(1);
+                double rectHeight = size.Height / model.Map.GetLength(0);
                 for (int i = 0; i < model.Map.GetLength(0); i++)
                 {
                     for (int j = 0; j < model.Map.GetLength(1); j++)
                     {
-                        ImageBrush brush = new ImageBrush();
-                        switch (model.Map[i,j])
+                        switch (model.Map[i, j])
                         {
                             case GameLogic.MapItem.player:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "player.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(PlayerBrush, null, new Rect(rectWidth * j, rectHeight * i, rectWidth, rectHeight));
                                 break;
                             case GameLogic.MapItem.wall:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images","wall.bmp"),UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(WallBrush, null, new Rect(rectWidth * j, rectHeight * i, rectWidth, rectHeight));
                                 break;
                             case GameLogic.MapItem.floor:
                                 break;
                             case GameLogic.MapItem.ammo:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "ammosingle.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(AmmoBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.opponent:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "opponent.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(OpponentBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.brick:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "brick.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(BrickBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.health:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "hp.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(HealthBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.locked:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "lock.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(LockedBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.exit:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "exit.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(ExitBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             case GameLogic.MapItem.finish:
-                                brush = new ImageBrush(new BitmapImage(new Uri(Path.Combine("Images", "finish.bmp"), UriKind.RelativeOrAbsolute)));
+                                drawingContext.DrawRectangle(FinishBrush, null, new Rect(rectWidth * j, rectHeight * i, rectHeight, rectWidth));
                                 break;
                             default:
                                 break;
                         }
-                        drawingContext.DrawRectangle(brush, new Pen(Brushes.Black, 0),
-                                    new Rect(j * itemWidth, i * itemHeight, itemWidth, itemHeight)
-                                    );
-                    }                   
+                    }
                 }
             }
         }
