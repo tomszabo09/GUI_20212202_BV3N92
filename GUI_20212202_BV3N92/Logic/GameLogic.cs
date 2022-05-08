502db1f5a0e6c6b79f54f0252eba4db63a891d97
zsiroszsombor <zsiroszsombor02@gmail.com>
2022-05-08 22:21:20 +0200

    Heavily modified logic to fit the new models

--- GUI_20212202_BV3N92/Logic/GameLogic.cs
+++ GUI_20212202_BV3N92/Logic/GameLogic.cs
@@ -7,18 +7,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GUI_20212202_BV3N92.Logic
 {
-    public class GameLogic : IGameModel, IGameControl
+    public enum ItemType
+    {
+        player, wall, floor, ammo, opponent, brick, health, locked, exit, finish
+    }
+    public class GameLogic : IGameModel
     {
 
         MainWindow mainWindow;
-
-        public enum MapItem
-        {
-            player, wall, floor, ammo, opponent, brick, health, locked, exit, finish
-        }
+        Size size;
+        public double rectWidth;
+        public double rectHeight;
 
         public enum Controls
         {
@@ -88,124 +91,124 @@ namespace GUI_20212202_BV3N92.Logic
             }
         }
 
-        public void Control(Controls control)
-        {
-            int i = player.Position[0];
-            int j = player.Position[1];
-            int old_i = i;
-            int old_j = j;
+        //public void Control(Controls control)
+        //{
+        //    int i = player.Position[0];
+        //    int j = player.Position[1];
+        //    int old_i = i;
+        //    int old_j = j;
 
-            switch (control)
-            {
-                case Controls.moveUp:
-                    if (i - 1 >= 0)
-                        i--;
-                    break;
-                case Controls.moveLeft:
-                    if (j - 1 >= 0)
-                        j--;
-                    break;
-                case Controls.moveDown:
-                    if (i + 1 < Map.GetLength(0))
-                        i++;
-                    break;
-                case Controls.moveRight:
-                    if (j + 1 < Map.GetLength(1))
-                        j++;
-                    break;
-                case Controls.rotateUp:
-                    player.Direction = Directions.up;
-                    break;
-                case Controls.rotateLeft:
-                    player.Direction = Directions.left;
-                    break;
-                case Controls.rotateDown:
-                    player.Direction = Directions.down;
-                    break;
-                case Controls.rotateRight:
-                    player.Direction = Directions.right;
-                    break;
-                case Controls.shoot:
-                    player.Shoot(player.Direction);
-                    break;
-                case Controls.menu:
-                    MenuWindow menu = new MenuWindow(mainWindow);
-                    menu.ShowDialog();
+        //    switch (control)
+        //    {
+        //        case Controls.moveUp:
+        //            if (i - 1 >= 0)
+        //                i--;
+        //            break;
+        //        case Controls.moveLeft:
+        //            if (j - 1 >= 0)
+        //                j--;
+        //            break;
+        //        case Controls.moveDown:
+        //            if (i + 1 < Map.GetLength(0))
+        //                i++;
+        //            break;
+        //        case Controls.moveRight:
+        //            if (j + 1 < Map.GetLength(1))
+        //                j++;
+        //            break;
+        //        case Controls.rotateUp:
+        //            player.Direction = Directions.up;
+        //            break;
+        //        case Controls.rotateLeft:
+        //            player.Direction = Directions.left;
+        //            break;
+        //        case Controls.rotateDown:
+        //            player.Direction = Directions.down;
+        //            break;
+        //        case Controls.rotateRight:
+        //            player.Direction = Directions.right;
+        //            break;
+        //        case Controls.shoot:
+        //            player.Shoot(player.Direction);
+        //            break;
+        //        case Controls.menu:
+        //            MenuWindow menu = new MenuWindow(mainWindow);
+        //            menu.ShowDialog();
 
-                    if (menu.ShowDialog() == true)
-                    {
-                        //save
-                        SaveLevel();
-                    }
-                    else
-                    {
-                        //restart
-                        GameLogic restart = new GameLogic(mainWindow);
-                    }
+        //            if (menu.ShowDialog() == true)
+        //            {
+        //                //save
+        //                SaveLevel();
+        //            }
+        //            else
+        //            {
+        //                //restart
+        //                GameLogic restart = new GameLogic(mainWindow);
+        //            }
 
-                    break;
-            }
+        //            break;
+        //    }
 
-            if (control == Controls.moveUp || control == Controls.moveLeft || control == Controls.moveDown || control == Controls.moveRight)
-            {
-                if (Map[i, j] == MapItem.floor)
-                {
-                    Map[i, j] = MapItem.player;
-                    Map[old_i, old_j] = MapItem.floor;
-                }
-                if (Map[i, j] == MapItem.health)
-                {
-                    if (player.Health < 3)
-                        player.Health++;
+        //    if (control == Controls.moveUp || control == Controls.moveLeft || control == Controls.moveDown || control == Controls.moveRight)
+        //    {
+        //        if (Map[i, j] == MapItem.floor)
+        //        {
+        //            Map[i, j] = MapItem.player;
+        //            Map[old_i, old_j] = MapItem.floor;
+        //        }
+        //        if (Map[i, j] == MapItem.health)
+        //        {
+        //            if (player.Health < 3)
+        //                player.Health++;
 
-                    Map[i, j] = MapItem.player;
-                    Map[old_i, old_j] = MapItem.floor;
-                }
-                if (Map[i, j] == MapItem.ammo)
-                {
-                    player.Ammo += 3;
+        //            Map[i, j] = MapItem.player;
+        //            Map[old_i, old_j] = MapItem.floor;
+        //        }
+        //        if (Map[i, j] == MapItem.ammo)
+        //        {
+        //            player.Ammo += 3;
 
-                    Map[i, j] = MapItem.player;
-                    Map[old_i, old_j] = MapItem.floor;
-                }
-                if (Map[i, j] == MapItem.exit)
-                {
-                    if (levels.Count > 0)
-                    {
-                        LoadLevel(levels.Dequeue(),false);
-                    }
-                    else
-                    {
-                        EndingWindow ending = new EndingWindow();
-                        ending.ShowDialog();
+        //            Map[i, j] = MapItem.player;
+        //            Map[old_i, old_j] = MapItem.floor;
+        //        }
+        //        if (Map[i, j] == MapItem.exit)
+        //        {
+        //            if (levels.Count > 0)
+        //            {
+        //                LoadLevel(levels.Dequeue(), false);
+        //            }
+        //            else
+        //            {
+        //                EndingWindow ending = new EndingWindow();
+        //                ending.ShowDialog();
 
-                        if (ending.ShowDialog() == true)
-                        {
-                            //restart
+        //                if (ending.ShowDialog() == true)
+        //                {
+        //                    //restart
 
-                            GameLogic restart = new GameLogic(mainWindow);
-                        }
-                        else
-                        {
-                            //exit
+        //                    GameLogic restart = new GameLogic(mainWindow);
+        //                }
+        //                else
+        //                {
+        //                    //exit
 
-                            mainWindow.Close();
-                            File.Delete("save.sav");
-                        }
-                    }
+        //                    mainWindow.Close();
+        //                    File.Delete("save.sav");
+        //                }
+        //            }
 
-                }
+        //        }
 
-            }
-            else if (control == Controls.shoot)
-            {
-                player.Shoot(player.Direction);
+        //    }
+        //    else if (control == Controls.shoot)
+        //    {
+        //        player.Shoot(player.Direction);
 
-                // TODO: implement map changes upon hitting objects
-            }
+        //        // TODO: implement map changes upon hitting objects
+        //    }
 
-            // TODO: implement opponent shooting at player
-        }
+        //    // TODO: implement opponent shooting at player
+        //}
 
         private void LoadLevel(string lvlPath, bool saved)
         {
@@ -213,18 +216,87 @@ namespace GUI_20212202_BV3N92.Logic
             {
                 string[] lines = File.ReadAllLines(lvlPath);
                 Map = new MapItem[int.Parse(lines[1]), int.Parse(lines[0])];
+                rectWidth = size.Width / Map.GetLength(1);
+                rectHeight = size.Height / Map.GetLength(0);
                 for (int i = 0; i < Map.GetLength(0); i++)
                 {
                     for (int j = 0; j < Map.GetLength(1); j++)
                     {
-                        Map[i, j] = ConvertToEnum(lines[i + 3][j]);
-
-                        if (Map[i, j] == MapItem.player)
+                        switch (lines[i + 2][j])
                         {
-                            player.Position = new int[] { i, j };
+                            case 'p':
+                                Map[i, j] = new Player()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'w':
+                                Map[i, j] = new Wall()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'a':
+                                Map[i, j] = new Ammo()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'o':
+                                var op = new Opponent()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                Map[i, j] = op;
+                                opponents.Add(op);
+                                break;
+                            case 'b':
+                                Map[i, j] = new Brick()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'h':
+                                Map[i, j] = new Health()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'l':
+                                Map[i, j] = new Lock()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'e':
+                                Map[i, j] = new Exit()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'f':
+                                Map[i, j] = new Finish()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            default:
+                                Map[i, j] = new Floor()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
                         }
-                        else if (Map[i, j] == MapItem.opponent)
-                            opponents.Add(new Opponent() { Position = new int[] { i, j } });
                     }
                 }
             }
@@ -232,18 +304,87 @@ namespace GUI_20212202_BV3N92.Logic
             {
                 string[] lines = File.ReadAllLines(lvlPath);
                 Map = new MapItem[int.Parse(lines[1]), int.Parse(lines[0])];
+                rectWidth = size.Width / Map.GetLength(1);
+                rectHeight = size.Height / Map.GetLength(0);
                 for (int i = 0; i < Map.GetLength(0); i++)
                 {
                     for (int j = 0; j < Map.GetLength(1); j++)
                     {
-                        Map[i, j] = ConvertToEnum(lines[i + 2][j]);
-
-                        if (Map[i, j] == MapItem.player)
+                        switch (lines[i + 2][j])
                         {
-                            player.Position = new int[] { i, j };
+                            case 'p':
+                                Map[i, j] = new Player()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'w':
+                                Map[i, j] = new Wall()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'a':
+                                Map[i, j] = new Ammo()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'o':
+                                var op = new Opponent()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                Map[i, j] = op;
+                                opponents.Add(op);
+                                break;
+                            case 'b':
+                                Map[i, j] = new Brick()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'h':
+                                Map[i, j] = new Health()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'l':
+                                Map[i, j] = new Lock()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'e':
+                                Map[i, j] = new Exit()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            case 'f':
+                                Map[i, j] = new Finish()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
+                            default:
+                                Map[i, j] = new Floor()
+                                {
+                                    X = rectWidth * j,
+                                    Y = rectHeight * i,
+                                };
+                                break;
                         }
-                        else if (Map[i, j] == MapItem.opponent)
-                            opponents.Add(new Opponent() { Position = new int[] { i, j } });
                     }
                 }
             }
@@ -267,34 +408,7 @@ namespace GUI_20212202_BV3N92.Logic
             }
 
             sw.Close();
-        }
-
-        private MapItem ConvertToEnum(char mapItem)
-        {
-            switch (mapItem)
-            {
-                case 'p':
-                    return MapItem.player;
-                case 'w':
-                    return MapItem.wall;
-                case 'a':
-                    return MapItem.ammo;
-                case 'o':
-                    return MapItem.opponent;
-                case 'b':
-                    return MapItem.brick;
-                case 'h':
-                    return MapItem.health;
-                case 'l':
-                    return MapItem.locked;
-                case 'e':
-                    return MapItem.exit;
-                case 'f':
-                    return MapItem.finish;
-                default:
-                    return MapItem.floor;
-            }
-        }
+        }       
 
         private void OnDeath(string currentLevel)
         {
--- GUI_20212202_BV3N92/Logic/IGameModel.cs
+++ GUI_20212202_BV3N92/Logic/IGameModel.cs
@@ -1,4 +1,5 @@
-﻿using static GUI_20212202_BV3N92.Logic.GameLogic;
+﻿using GUI_20212202_BV3N92.Models;
+using static GUI_20212202_BV3N92.Logic.GameLogic;
 
 namespace GUI_20212202_BV3N92.Logic
 {
